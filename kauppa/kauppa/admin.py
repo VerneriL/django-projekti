70a4ab9cb72643ee76f474e53f8b5a71f2efd2d4
Verneri <44767@edu.raseko.fi>
2021-09-20 12:57:28 +0300

    Lisää hinta-kenttä Tuote-malliin

--- kauppa/kauppa/admin.py
+++ kauppa/kauppa/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from .models import Tuote
 
 class TuoteAdmin(admin.ModelAdmin):
-    fields = ["nimi"]
+    fields = ["nimi", "hinta"]
 
 admin.site.register(Tuote, TuoteAdmin)
--- kauppa/kauppa/models.py
+++ kauppa/kauppa/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Tuote(models.Model):
     nimi = models.CharField(max_length=100)
+    hinta = models.DecimalField(max_digits=9, decimal_places=2)
 
     def __str__(self):
         return f"Tuote: {self.nimi}"
